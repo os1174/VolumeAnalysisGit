3382d4f6ea6d59d216133bfe3c94712eb8dff966
os1174 <os1174@messiah.edu>
2023-12-08 14:23:17 -0500

    102Hz Stuff

--- CalculateVolume.m
+++ CalculateVolume.m
@@ -12,17 +12,16 @@ function [pumpLiters] = CalculateVolume(pumpingMovement,pumpSeconds)
     %pumpLiters = ((-b - sqrt((b*b) - (4 * (a) * (c - (timePerRad))))) / (2*a)) * pumpingMovement;
     %sendDebugMessage("Single Equation Volume Pumped = ", pumpLiters);  //Debug
     
-    if (timePerRad > 2.25)        %If determined to be in slow pumping cluster
+    pumpLiters = 0;
+
+    if (timePerRad >= 2.25)        %If determined to be in slow pumping cluster
         pumpLiters = (.0227*(timePerRad)+.2571)*pumpingMovement;
     end
-    if (timePerRad < 2.25 && timePerRad > 1.75)    %If determined to be in medium pumping cluster
+    if (timePerRad < 2.25 && timePerRad >= 1.75)    %If determined to be in medium pumping cluster
         pumpLiters = (.0128*(timePerRad)+.3208)*pumpingMovement;
     end
     if (timePerRad < 1.75)             %If determined to be in fast pumping cluster
         pumpLiters = (.0106*(timePerRad)+.4138)*pumpingMovement;
-    end
-    if(pumpLiters < 0)  % saw this during debug of code need to find out why this can happen
-        pumpLiters = 0;
     end
    
 end
